abec15408d7e69ee3a2bcecaf69e0fb8339a7a5d
Romaric250 <78319422+Romaric250@users.noreply.github.com>
2023-08-11 21:55:03 -1100

    Update index.js

--- mercedez-projects/project 2.0/index.js
+++ mercedez-projects/project 2.0/index.js
@@ -32,26 +32,35 @@ window.onload = ()=>{
 }
 
 
-var swiper = new Swiper(".books-slider", {
-   loop:true,
-   centeredSlides:true,
-   autoplay:{
-    delay: 9500,
-    disableOnInteraction: false,
-   },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
+var swiper = new Swiper(".books-slider",  {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+     delay: 9500,
+     disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+     breakpoints: {
+       0: {
+         slidesPerView: 1,
+       },
+       450: {
         slidesPerView: 2,
       },
-      1024: {
-        slidesPerView: 3,
+       768: {
+         slidesPerView: 3,
+       },
+
+       1024: {
+        slidesPerView: 4,
       },
-    },
-    allowTouchMove:false
-  });
+     },
+      allowToucMove:false
+   });
   var swiper = new Swiper(".featured-slider", {
     spaceBetween: 10,
     loop:true,
