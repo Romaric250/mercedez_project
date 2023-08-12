f8ab69dccf9b408bef2359eb3906563b389af83c
Romaric250 <78319422+Romaric250@users.noreply.github.com>
2023-08-11 21:59:11 -1100

    Update index.js

--- mercedez-projects/project 2.0/index.js
+++ mercedez-projects/project 2.0/index.js
@@ -36,7 +36,7 @@ var swiper = new Swiper(".books-slider",  {
     loop:true,
    centeredSlides:true,
    autoplay:{
-    delay: 9500,
+    delay:40000,
     disableOnInteraction: false,
    },
     breakpoints: {
